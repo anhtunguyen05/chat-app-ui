c80dbfbe069862d84fd24028d9b27a70931b5fe9
tudeptrai <anhtunguyen643@gmail.com>
2025-10-10 16:36:50 +0700

    feat: implement change friend status button integrated with API #31

--- src/services/friendService.ts
+++ src/services/friendService.ts
@@ -5,7 +5,41 @@ interface FriendResponse {
   data: User[];
 }
 
+interface FriendActionResponse {
+  message: string;
+  user: User;
+}
+
+interface CancelResponse{
+  message: string;
+}
+
 export async function getFriendList(): Promise<User[]> {
   const res = await api.get<FriendResponse>("/friends");
   return res.data.data;
+}
+
+export async function sendFriendRequest(toId: string): Promise<FriendActionResponse> {
+  const res = await api.post<FriendActionResponse>(`/friends/request/${toId}`);
+  return res.data;
+}
+
+export async function acceptFriendRequest(fromId: string): Promise<FriendActionResponse> {
+  const res = await api.put<FriendActionResponse>(`/friends/accept/${fromId}`);
+  return res.data;
+}
+
+export async function rejectFriendRequest(fromId: string): Promise<FriendActionResponse> {
+  const res = await api.put<FriendActionResponse>(`/friends/reject/${fromId}`);
+  return res.data;
+}
+
+export async function cancelFriendRequest(toId: string): Promise<CancelResponse> {
+  const res = await api.delete<CancelResponse>(`/friends/cancel/${toId}`);
+  return res.data;
+}
+
+export async function unfriend(userId: string): Promise<CancelResponse> {
+  const res = await api.delete<CancelResponse>(`/friends/unfriend/${userId}`);
+  return res.data;
 }
